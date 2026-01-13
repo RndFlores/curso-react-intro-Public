c74ab2db510c020bc1abfb8e118c1353f2cb0996
ronaldo3552 <ronaldodylan96@gmail.com>
2026-01-12 19:31:08 -0500

    Eventos sencillos en CreateTodoButton y TodoSearch

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,6 +1,15 @@
+import "./CreateTodoButton.css";
 function CreateTodoButton(){
     return(
-        <button>+</button>
+        <button className="CreateTodoButton"
+        onClick={
+        (event)=>{
+            console.log('le diste click')
+            console.log(event)
+            console.log(event.target)
+        }
+        }
+        >+</button>
     );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,13 @@ import "./TodoSearch.css";
 
 function TodoSearch() {
   return(
-    <input className="TodoSearch" placeholder="Buscar TODO" />
+    <input className="TodoSearch" placeholder="Buscar TODO"
+    onChange={(event)=>{
+      console.log('Escribiste en TodoSearch')
+      console.log(event) 
+      console.log(event.target.value)      
+    }}
+    />
   );
 
 }
