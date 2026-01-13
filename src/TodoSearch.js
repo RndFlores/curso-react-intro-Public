013d117dfd0742b564444c682a86056a27f81b68
ronaldo3552 <ronaldodylan96@gmail.com>
2026-01-12 19:58:35 -0500

    Entrado a los States

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,12 +2,19 @@ import React from 'react';
 import "./TodoSearch.css";
 
 function TodoSearch() {
+
+  //herramienta de react
+  const [searchValue, setSearchValue]=React.useState('');// no sloamente se consume tmb se actualiza
+
+  console.log('Los usuarios buscan: ' + searchValue);
   return(
     <input className="TodoSearch" placeholder="Buscar TODO"
+    value ={searchValue}
     onChange={(event)=>{
       console.log('Escribiste en TodoSearch')
-      console.log(event) 
-      console.log(event.target.value)      
+      // console.log(event) 
+      // console.log(event.target.value)
+      setSearchValue(event.target.value);      
     }}
     />
   );
